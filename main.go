a4ec755eb9b0274d652808cd34be603f974bab5e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-01-28 22:17:47 +0100

    picking an alive backend

--- main.go
+++ main.go
@@ -26,6 +26,23 @@ func (sp *ServerPool) NextIndex() int {
 	return int(atomic.AddUint64(&sp.current, uint64(1)) % uint64(len(sp.backends)))
 }
 
+// GetNextPeer returns next active perrt to take a connection
+func (sp *ServerPool) GetNextPeer() *Backend {
+	// loop entire backends to find out an Alive one
+	next := sp.NextIndex()
+	l := len(sp.backends) + next // start from next add move a full cycle
+	for i := next; i < l; i++ {
+		idx := i % len(sp.backends) // start from next and move a full cycle
+		if sp.backends[idx].Alive { // if we have an alive backend, use it and store if its not the original one
+			if idx != next {
+				atomic.StoreUint64(&sp.current, uint64(idx))
+			}
+			return sp.backends[idx]
+		}
+	}
+	return nil
+}
+
 func main() {
 	u, _ := url.Parse("http://localhost:8080")
 	rp := httputil.NewSingleHostReverseProxy(u)
